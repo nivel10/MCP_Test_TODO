1d00cad8fbc8c7d7fad758e59191d458e86369d9
Carlos H. <carlos.e.herrera.j@gmail.com>
2025-04-18 12:28:25 -0400

    Sync - 04/18/2025 - 12:28
    Sync - 04/18/2025 - 12:28

---
+++ src/index.ts
@@ -0,0 +1,70 @@
+import { McpServer, } from '@modelcontextprotocol/sdk/server/mcp.js';
+import { StdioServerTransport, } from '@modelcontextprotocol/sdk/server/stdio.js';
+import { z, } from 'zod';
+
+const server = new McpServer({
+    name: 'TODO',
+    version: '1.0.0',
+    description: 'MCP server test - TODO',
+});
+
+server.tool(
+    'add-todo',
+    {
+        text: z.string(),
+    },
+    async ({ text }) => {
+        return {
+            content: [
+                {
+                    type: 'text',
+                    text: `${text} was added to our todo with id 99`,
+                }
+            ],
+        }
+    }
+);
+
+server.tool(
+    'get-todo',
+    {},
+    async () => {
+        return {
+            content: [
+                {
+                    type: 'text',
+                    text: `Call mom ID 99`
+                }
+            ]
+        }
+    }
+);
+
+server.tool(
+    'remove-todo',
+    {
+        id: z.number(),
+    },
+    async ({ id }) => {
+        return {
+            content: [
+                {
+                    type: 'text',
+                    text: `Todo ${id} was removed`,
+                }
+            ]
+        }
+    }
+);
+
+async function main() {
+    try {
+        const transport = new StdioServerTransport();
+        await server.connect(transport);
+    } catch (ex) {
+        console.error(ex);
+        process.exit(1);
+    }
+}
+
+main();
